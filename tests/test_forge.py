5a6f3e6f9eeb79fed75501006f8801b0552943d5
Jesus Renero Quintero <jesus.renero@gmail.com>
2024-03-11 08:50:30 +0100

    fix pytest error

--- tests/test_forge.py
+++ tests/test_forge.py
@@ -1,12 +1,16 @@
-from forge import Pipeline
+"""
 
+Tests for the forge module.
 
+"""
 # pylint: disable=E1101:no-member, W0201:attribute-defined-outside-init, W0511:fixme
 # pylint: disable=C0103:invalid-name, W0212:protected-access
 # pylint: disable=C0116:missing-function-docstring, C0115:missing-class-docstring
 # pylint: disable=R0913:too-many-arguments, R0903:too-few-public-methods
 # pylint: disable=R0914:too-many-locals, R0915:too-many-statements
 # pylint: disable=W0106:expression-not-assigned, R1702:too-many-branches
+
+from forge import Pipeline
 
 
 # Used to test access to global classes
