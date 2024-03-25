1589e013790569f9598707be82aff21b784e043e
Jesus Renero Quintero <jesus.renero@gmail.com>
2024-03-25 16:19:30 +0100

    path refined

--- tests/test_build_params.py
+++ tests/test_build_params.py
@@ -19,7 +19,7 @@ import pytest
 
 
 sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
+    os.path.join(os.path.dirname(__file__), '../mlforge')))
 
 from mlforge import Pipeline
 
