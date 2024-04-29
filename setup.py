d00aeb064eb676f47e262fd3ebf1f43603feb385
Jesus Renero Quintero <jesus.renero@gmail.com>
2024-04-29 09:33:20 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mlforge",
-    version="0.1.1-alpha",
+    version="0.1.2a",
     author="J. Renero",
     author_email="jesus.renero@gmail.com",
     description="A package to design and run sequential ML pipelines",
